2c51e01bff54a3b1298da8422ea77c1c8e353d25
testsfortests <official.testsfortests@gmail.com>
2026-08-06 02:32:15 +0530

    pwa logo added

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,12 @@ import "./globals.css";
 export const metadata = {
   title: "Member Mate",
   description: "Member Mate App",
+  manifest: "/manifest.json",
+  themeColor: "#00ff00",
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/icons-180.png",
+  },
 };
 
 export default function RootLayout({ children }) {
